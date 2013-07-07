578419048309c44e9e340c942bb99caadbc846c1
cmichi <mich@elmueller.net>
2013-07-07 21:06:01 +0200

    updates with first slides

--- chaosseminar-open-data/slide_config.js
+++ chaosseminar-open-data/slide_config.js
@@ -2,17 +2,16 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'Open Data',
-    subtitle: 'ChaosSeminar Juli 2013',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    //subtitle: 'ChaosSeminar Juli 2013',
+    eventInfo: {
+      title: 'ChaosSeminar Ulm',
+      date: '8. Juli 2013'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    //favIcon: 'images/google_developers_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -27,6 +26,6 @@ var SLIDE_CONFIG = {
     twitter: '@cmichi',
     www: 'http://micha.elmueller.net',
     github: 'http://github.com/cmichi'
-  }
+  }]
 };
 
