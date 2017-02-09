2f1611c07c9b477106321c026e550b809b8b7be3
cmichi <mich@elmueller.net>
2017-02-09 21:00:25 +0100

    add welcome day talk

---
+++ welcome-day/slide_config.js
@@ -0,0 +1,30 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Michael M&uuml;ller',
+    subtitle: '<span class="uni-red">#node.js&nbsp;&nbsp;#unix&nbsp;&nbsp;#ui&nbsp;&nbsp;<span style="">#typography</span></span><br/>www:&nbsp;&nbsp;&nbsp;http://micha.elmueller.net<br/>github: &commat;cmichi',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/uulm.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['default', 'uulm-changes'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+	{
+		name: ''
+	}
+  ]
+};
+
